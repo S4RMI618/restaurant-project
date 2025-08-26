010886fa7b8b2ab6a99d78d0e4265fe11ad4e08a
S4RMI618 <huessabe@gmail.com>
2025-08-25 22:46:02 -0500

    first commit

---
+++ projectpooii/src/main/java/com/example/projectpooii/ProjectpooiiApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectpooii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectpooiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectpooiiApplication.class, args);
+	}
+
+}
---
+++ projectpooii/src/test/java/com/example/projectpooii/ProjectpooiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectpooii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectpooiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
